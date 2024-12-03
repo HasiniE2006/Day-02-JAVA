d3aff2df547f2746714da8ed0c397a3bd3659a24
HasiniE2006 <hasini1530@gmail.com>
2024-12-03 10:10:06 +0530

    Add files via upload

---
+++ Arithemeticoperators.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Arithemeticoperators
+{
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a:");
+        a=sc.nextInt();
+        System.out.println("enter b:");
+        b=sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
