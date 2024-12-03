f1c5efbd646660e89916c30bd20822fd5fc8f63a
HasiniE2006 <hasini1530@gmail.com>
2024-12-03 12:29:26 +0530

    Add files via upload

---
+++ Swapwithouttemp1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Swapwithouttemp1
+{
+    public static void main(String[] args) {
+        int boy,girl;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter boy choclates");
+        boy=sc.nextInt();
+        System.err.println("enter girl choclates");
+        girl=sc.nextInt();
+        boy=boy*girl;
+        girl=boy/girl;
+        boy=boy/girl;
+        System.out.println(boy);
+        System.out.println(girl);
+    
+    }
+    
+}
