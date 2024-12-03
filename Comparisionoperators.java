bc2da719bf6584995ff07c3ffcf6dde441a604c7
HasiniE2006 <hasini1530@gmail.com>
2024-12-03 10:25:29 +0530

    Add files via upload

---
+++ Comparisionoperators.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner;
+class Comparisionoperator
+{
+   public static void main(String[] args)
+   {
+    int a,b;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("enter a:");
+      a=sc.nextInt();
+      System.out.println("enter b:");
+      b=sc.nextInt();
+      System.out.println(a==b);
+      System.out.println(a!=b);
+      System.out.println(a>=b);
+      System.out.println(a<=b);
+      System.out.println(a>b);
+      System.out.println(a<b);
+   }
+}
+
