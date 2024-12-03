0e1a1583509943e1b51668a921bbb14f8481b275
HasiniE2006 <hasini1530@gmail.com>
2024-12-03 14:16:52 +0530

    Add files via upload

---
+++ Logicaloperators.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Logicaloperators {
+    public static void main(String[] args) {
+    int a,b;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a:");
+    a=sc.nextInt();
+    System.out.println("enter b:");
+    b=sc.nextInt();
+    System.out.println(a>b && a<b);
+    System.out.println(a>b||a<b);
+    System.out.println(!(a<b));
+    System.out.println(!(a<b));
+    }
+}
+
+
